5be271f2c2bc8ee00a710fc81709047d7587da7e
PingjunChen <pingjunchen@ieee.org>
2021-12-20 13:07:48 -0600

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('.'))
 from sphinx.application import Sphinx
-import myst_parser
 
 
 # -- Project information -----------------------------------------------------
@@ -32,7 +31,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst_parser',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
