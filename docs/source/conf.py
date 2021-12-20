c48cc8fb5b197c54404f75f5a280209304d25f60
PingjunChen <pingjunchen@ieee.org>
2021-12-20 12:59:31 -0600

    update configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+
+import os
+import sys
+from sphinx.application import Sphinx
+from sphinx.util.docutils import SphinxDirective
+from myst_parser import __version__
 # sys.path.insert(0, os.path.abspath('.'))
-import myst_parser
 
 
 # -- Project information -----------------------------------------------------
